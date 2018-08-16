85f252f20fec93f3b9f974bcd0f4688f4dd2e6c7
Diane <d.bunod@gmail.com>
2018-08-16 15:57:25 +0200

    diane V1

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def index_2
+  end
+
+  def index_3
+  end 
+
+  def who
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
